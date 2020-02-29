73832b66aac19a87d72f3d3624b795a6fd785821
Johan Björk <johanimon@gmail.com>
2020-02-29 18:35:46 +0100

    Add CORS support

--- server/src/main.kt
+++ server/src/main.kt
@@ -9,6 +9,12 @@ import leif.http.ListVerificationsAction
 import leif.http.LoginAction
 import spark.Service
 
+private val CORS_HEADERS = mapOf(
+    "Access-Control-Allow-Headers" to "Accept, Content-Type, Access-Token",
+    "Access-Control-Allow-Methods" to "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Origin" to "*"
+)
+
 fun main() {
     val config = ApplicationConfig {
         httpHost = "127.0.0.1"
@@ -27,6 +33,14 @@ fun main() {
             "app" to "leif",
             "version" to "1.0"
         )
+    }
+    http.options("/*") { _, response ->
+        response.status(200)
+    }
+    http.afterAfter { _, response ->
+        CORS_HEADERS.forEach { pair ->
+            response.header(pair.key, pair.value)
+        }
     }
     http.post("/8da3cf8a0a1111ef367cb563181a175864d48cd2e95d54c9839aa1233b78eba1", InitAction(app))
     http.post("/login", LoginAction(app))
