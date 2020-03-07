6c5d6344369ba4dcb05e3a0b1b0535f2afb9090e
Johan Björk <johanimon@gmail.com>
2020-03-08 00:24:53 +0100

    Remove MySQL stuff from CI and db test class

--- server/test/DatabaseTest.kt
+++ server/test/DatabaseTest.kt
@@ -1,25 +1,21 @@
 package leif
 
-import leif.config.VariableResolver
 import leif.database.Database
-import java.nio.file.Files
-import java.nio.file.Path
 import leif.database.JDBCDatabase
-import org.junit.jupiter.api.BeforeEach
+
+private fun loadTableSchema(db: Database) {
+    val schema = object {}.javaClass.getResourceAsStream("/sqlite.sql")
+        .readAllBytes()
+        .toString(Charsets.UTF_8)
+
+    schema
+        .split(";")
+        .map(String::trim)
+        .filter { it.isNotEmpty() }
+        .forEach { db.statement(it) }
+}
 
 abstract class DatabaseTest {
-    companion object {
-        val DB_NAME = VariableResolver("leif-test") {
-            environment("DB_NAME")
-        }
-        val DB_USERNAME = VariableResolver("root") {
-            environment("DB_USERNAME")
-        }
-        val DB_PASSWORD = VariableResolver("") {
-            environment("DB_PASSWORD")
-        }
-    }
-
     private var internalDb: JDBCDatabase? = null
 
     val db: JDBCDatabase by lazy {
@@ -31,17 +27,5 @@ abstract class DatabaseTest {
 
     fun getTableRows(table: String): List<Map<String, Any?>> {
         return db.select("SELECT * FROM $table")
-    }
-
-    private fun loadTableSchema(db: Database) {
-        val schema = this.javaClass.getResourceAsStream("/sqlite.sql")
-            .readAllBytes()
-            .toString(Charsets.UTF_8)
-
-        schema
-            .split(";")
-            .map(String::trim)
-            .filter { it.isNotEmpty() }
-            .forEach { db.statement(it) }
     }
 }
