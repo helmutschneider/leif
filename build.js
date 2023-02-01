c8a2260d1f2943a8bf8ff37141c9c9a27a5bf322
Johan Björk <johanimon@gmail.com>
2023-02-01 20:34:56 +0100

    Upgrade esbuild among other dependencies

--- build.js
+++ build.js
@@ -1,31 +1,55 @@
+const process = require('process');
 const child = require('child_process');
 const esbuild = require('esbuild');
 const isProduction = process.env.NODE_ENV === 'production';
 
-esbuild.build({
+const context = esbuild.context({
     bundle: true,
     entryPoints: ['client/app.tsx'],
     minify: isProduction,
     outfile: 'public/app.js',
     platform: 'browser',
+    plugins: [
+        {
+            name: 'do_something_on_rebuild',
+            setup: build => {
+                build.onEnd(result => {
+                    const dt = (new Date()).toISOString();
+                    const error = result.errors
+                        .map(e => String(e))
+                        .join('\n');
+
+                    if (error) {
+                        console.error(`[${dt}] esbuild error: ${error}`);
+                    } else {
+                        console.log(`[${dt}] esbuild OK`);
+                    }
+                });
+            },
+        },
+    ],
     sourcemap: true,
     target: 'es2017',
-    watch: isProduction ? false : {
-        onRebuild: (error) => {
-            const dt = (new Date()).toISOString();
-            if (error) {
-                console.error(`[${dt}] esbuild error: ${error}`);
-            } else {
-                console.log(`[${dt}] esbuild OK`);
-            }
-        },
-    },
-}).catch(err => {
-    console.error(err);
-    process.exit(1);
 });
 
+if (isProduction) {
+    context.then(async c => {
+        await c.rebuild();
+        await c.dispose();
+    });
+}
+
 if (!isProduction) {
+    context.then(c => {
+        console.log('Watching!');
+
+        process.on('SIGINT', async event => {
+            await c.dispose();
+        });
+
+        return c.watch();
+    });
+
     // typescript compiler
     const tsc = child.spawn('node', ['./node_modules/.bin/tsc', '--watch', '--project', 'tsconfig.json']);
 
@@ -37,9 +61,13 @@ if (!isProduction) {
         console.error(data.toString())
     });
 
-// php web server
-    child.spawn('php', ['-S', 'localhost:8000', '-t', 'public'], {
+    // php web server
+    const php = child.spawn('php', ['-S', 'localhost:8000', '-t', 'public'], {
         stdio: 'inherit',
     });
-}
 
+    process.on('SIGINT', event => {
+        tsc.kill('SIGINT');
+        php.kill('SIGINT');
+    });
+}
