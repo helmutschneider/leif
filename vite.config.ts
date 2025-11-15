222e8af75a6392b5924d7d2891a73eab831ebca9
Johan Björk <johanimon@gmail.com>
2025-11-15 17:53:17 +0100

    Add a shitty vite plugin for the PHP backend

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig, loadEnv } from "vite";
 import path from "path";
+import child from "child_process";
 import react from "@vitejs/plugin-react";
 import checker from "vite-plugin-checker";
+
+function phpServer(config) {
+  let proc;
+
+  return {
+    name: "php-server",
+    buildStart: (x) => {
+      if (config.mode === "production") {
+        return;
+      }
+      if (proc) {
+        return;
+      }
+      proc = child.spawn('php', ['-S', 'localhost:8000', '-t', 'public'], {
+        stdio: 'inherit',
+      });
+    },
+  }
+}
 
 export default defineConfig((config) => {
   const env = loadEnv(config.mode, process.cwd(), "");
@@ -17,6 +37,7 @@ export default defineConfig((config) => {
         },
         overlay: false,
       }),
+      phpServer(config),
     ],
     define: {
       "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV),
